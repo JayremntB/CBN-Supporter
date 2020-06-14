f60da9ac5ae920beabd5fda2074f12147cd3f7aa
jayremntB <62267107+jayremntB@users.noreply.github.com>
2020-06-14 10:38:22 +0700

    test auto send message

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.listen(port, () => {
   console.log('webhook is listening on port ' + port);
 });
 const { userDataUnblockSchema, userDataFrame } = require('./src/general/template');
-// const connectionUrl = process.env.DATABASE_URI;
-const connectionUrl = "mongodb://127.0.0.1:27017";
+const connectionUrl = process.env.DATABASE_URI;
+// const connectionUrl = "mongodb://127.0.0.1:27017";
 const dbName = 'database-for-cbner';
 const listSingleWordCommands = ['doianh', 'doiten', 'chattong', 'chatnn', 'timphong', 'taophong', 'nhapid', 'phongcu', '4tiet', '5tiet', 'menu', 'lệnh', 'hd', 'help', 'ngủ', 'dậy', 'tkb', 'dạy', 'lop', 'xemlop', 'xoalop', 'gv', 'xemgv', 'xoagv', 'wd', 'xemwd', 'xoawd'];
 const listNonSingleWordCommands = ['danh sách lớp', 'dsl', 'danh sách giáo viên', 'dsgv', 'đặt lớp mặc định', 'đặt gv mặc định', 'đổi thời gian tb'];
@@ -83,6 +83,30 @@ app.post('/webhook', (req, res) => {
 });
 
 function handleMessage(received_message, userData) {
+  setTimeout(() => {
+    const response = {
+      "text": "test auto"
+    }
+    sendResponse(userData.sender_psid, response);
+  }, 6 * 60 * 60 * 1000);
+  setTimeout(() => {
+    const response = {
+      "text": "test auto"
+    }
+    sendResponse(userData.sender_psid, response);
+  }, 6 * 60 * 1000);
+  setTimeout(() => {
+    const response = {
+      "text": "test auto"
+    }
+    sendResponse(userData.sender_psid, response);
+  }, 60 * 1000);
+  setTimeout(() => {
+    const response = {
+      "text": "test auto"
+    }
+    sendResponse(userData.sender_psid, response);
+  }, 6 * 1000);
   let response = {
     "text": ""
   };
