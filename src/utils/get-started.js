4361c917e5ba6c5c5158135643b4563098dc055d
jayremntB <62267107+jayremntB@users.noreply.github.com>
2020-07-17 14:47:40 +0700

    modify get started

--- src/utils/get-started.js
+++ src/utils/get-started.js
@@ -2,24 +2,26 @@ const sendResponse = require('../general/sendResponse');
 const typing = require('../general/typing');
 
 module.exports = function(sender_psid) {
-  let response = {
-    "text": "Xin chào! Tớ tên Jay, rất vui được gặp bạn :D Tớ được thiết lập sẵn để cung cấp cho bạn các tính năng có trong Menu, cứ thoải mái vung tay mà sử dụng nhé :>"
-  };
-  sendResponse(sender_psid, response);
-  typing(sender_psid);
-  setTimeout(()=> {
-    response = {
-      "text": "Nếu bạn cần hỗ trợ hay muốn góp ý, gợi ý một số tính năng nào đó cho tớ, hãy liên hệ với thằng coder qua phần Hỗ trợ dưới Menu hoặc nhập Help nha 😚\nĐược rồi, nhập Menu để bắt đầu sử dụng nhé 😉",
-      "quick_replies": [
-        {
-          "content_type": "text",
-          "title": "Menu",
-          "payload": "menu",
-          "image_url": ""
-        }
-      ]
+  setTimeout(() => {
+    let response = {
+      "text": "Xin chào! Tớ tên Jay, rất vui được gặp bạn :D Tớ được thiết lập sẵn để cung cấp cho bạn các tính năng có trong Menu, cứ thoải mái vung tay mà sử dụng nhé :>"
     };
     sendResponse(sender_psid, response);
     typing(sender_psid);
-  }, 3100);
+    setTimeout(()=> {
+      response = {
+        "text": "Nếu bạn cần hỗ trợ hay muốn góp ý, gợi ý một số tính năng nào đó cho tớ, hãy liên hệ với thằng coder qua phần Hỗ trợ dưới Menu hoặc nhập Help nha 😚\nĐược rồi, nhập Menu để bắt đầu sử dụng nhé 😉",
+        "quick_replies": [
+          {
+            "content_type": "text",
+            "title": "Menu",
+            "payload": "menu",
+            "image_url": ""
+          }
+        ]
+      };
+      sendResponse(sender_psid, response);
+      typing(sender_psid);
+    }, 3100);
+  }, 3000);
 }
