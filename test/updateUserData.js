28b13845394bce2aaac196a01ce2779db3850d7d
Nguyễn Duy Bảo <jayremntb@gmail.com>
2020-09-06 14:16:13 +0700

    production

--- test/updateUserData.js
+++ test/updateUserData.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require('mongodb');
+const request = require('request');
 // const connectionUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-obtbe.mongodb.net/test`;
 // const connectionUrl = "mongodb://127.0.0.1:27017"
 const connectionUrl = process.env.DATABASE_URI;
 const dbName = "database-for-cbner";
-MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, async (err, client) => {
   if(err) {
     return console.log('Failed to connect to database');
   }
@@ -12,12 +13,35 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
   date.setHours(date.getHours() + 7); // deploy at US
   const timeNow = date.getTime();
   // update
-  client.db(dbName).collection('users-data').updateMany({}, {
-    $set: {
-      // simsimi_lang: "vi" // simsimi update
-    }
-  }, (err) => {
-    if(err) console.error(err);
-    else console.log("successfully updated!");
-  });
+  // client.db(dbName).collection('users-data').updateMany({}, {
+  //   $set: {
+  //     // simsimi_lang: "vi" // simsimi update
+  //   }
+  // }, (err) => {
+  //   if(err) console.error(err);
+  //   else console.log("successfully updated!");
+  // });
+  // add name
+  let usersData = await client.db(dbName).collection('users-data').find({}).toArray();
+  usersData.forEach(userData => {
+    request({
+      "uri": `https://graph.facebook.com/${userData.sender_psid}`,
+      // "qs": { "access_token": process.env.TEST_PAGE_ACCESS_TOKEN },
+      "qs": {
+        "access_token": process.env.PAGE_ACCESS_TOKEN,
+        "fields": "name,profile_pic"
+      },
+      "method": "GET"
+    }, (err, res, body) => {
+      body = JSON.parse(body);
+      console.log(body.name);
+      client.db(dbName).collection('users-data').updateOne({ sender_psid: userData.sender_psid }, {
+        $set: {
+          name: body.name
+        }
+      }, (err) => {
+        console.log(userData.sender_psid + " -> " + body.name);
+      });
+    });
+  })
 });
