ae775113c4e4b5f3a4accc479427da6fbd1d276e
Nguyễn Duy Bảo <jayremntb@gmail.com>
2020-09-13 08:51:13 +0700

    fix detect feature error

--- app.js
+++ app.js
@@ -139,20 +139,6 @@ function handleMessage(received_message, userData) {
       unblockAll(userData);
       response = textResponse.exitResponse;
     }
-    else if(keySearchSchedule) {
-      searchSchedule.init(client, userData);
-    }
-    else if(keySearchClasses) {
-      searchClasses.init(client, userData);
-    }
-    else if(keyChatRoom) {
-      unblockAll(userData);
-      response = templateResponse.chatRoom;
-    }
-    else if(keyCheckCovid) {
-      unblockAll(userData);
-      checkCovid(userData.sender_psid);
-    }
     else if(listNonSingleWordCommands.includes(text)) {
       if(userData.live_chat) {
         liveChat.deniedUsingOtherFeatures(userData);
@@ -272,6 +258,20 @@ function handleMessage(received_message, userData) {
     }
     else if(userData.room_chatting.block) {
       chatRoom.handleMessage(client, defaultText, userData);
+    }
+    else if(keySearchSchedule) {
+      searchSchedule.init(client, userData);
+    }
+    else if(keySearchClasses) {
+      searchClasses.init(client, userData);
+    }
+    else if(keyChatRoom) {
+      unblockAll(userData);
+      response = templateResponse.chatRoom;
+    }
+    else if(keyCheckCovid) {
+      unblockAll(userData);
+      checkCovid(userData.sender_psid);
     }
   }
   else if(received_message.attachments) {
