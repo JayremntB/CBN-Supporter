129115efe0d15890f7ddf696158524ba52be7307
Nguyễn Duy Bảo <jayremntb@gmail.com>
2021-01-05 07:43:05 +0700

    test

--- test/updateUserData.js
+++ test/updateUserData.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
 const request = require('request');
 // const connectionUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-obtbe.mongodb.net/test`;
-const connectionUrl = "mongodb://127.0.0.1:27017"
-// const connectionUrl = process.env.DATABASE_URI;
+// const connectionUrl = "mongodb://127.0.0.1:27017"
+const connectionUrl = process.env.DATABASE_URI;
 const dbName = "database-for-cbner";
 MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, async (err, client) => {
   if(err) {
@@ -13,14 +13,14 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
   date.setHours(date.getHours() + 7); // deploy at US
   const timeNow = date.getTime();
   // update
-  // client.db(dbName).collection('users-data').updateMany({}, {
-  //   $set: {
-  //     // simsimi_lang: "vi" // simsimi update
-  //   }
-  // }, (err) => {
-  //   if(err) console.error(err);
-  //   else console.log("successfully updated!");
-  // });
+  client.db(dbName).collection('users-data').updateMany({}, {
+    $set: {
+      "room_chatting.persona_id": "3363745553659185"
+    }
+  }, (err) => {
+    if(err) console.error(err);
+    else console.log("successfully updated!");
+  });
 
   // add name
   // let usersData = await client.db(dbName).collection('users-data').find({}).toArray();
@@ -47,36 +47,36 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
   // });
 
   // kick all users
-  const listRooms = await client.db(dbName).collection('room-chatting').find({
-    "list_users.0": {
-      $exists: true
-    }
-  }).toArray();
-  console.log("ok");
-  listRooms.forEach(room => {
-    room.list_users.forEach(sender_psid => {
-      client.db(dbName).collection('users-data').updateOne({ sender_psid: sender_psid }, {
-        $set: {
-          "room_chatting.block": false,
-          "room_chatting.has_joined": false,
-          "room_chatting.type": "",
-          "room_chatting.room_id": "",
-          "room_chatting.create_new_subroom": false
-        }
-      }, (err) => {
-        if(err) console.log(err);
-        else console.log(sender_psid + " ok");
-      });
-    });
-    client.db(dbName).collection("room-chatting").updateOne({
-      "room_id": room.room_id
-    }, {
-      $set: {
-        "list_users": []
-      }
-    }, (err) => {
-      if(err) console.log(err);
-      else console.log(room.room_id + " ok");
-    })
-  });
+  // const listRooms = await client.db(dbName).collection('room-chatting').find({
+  //   "list_users.0": {
+  //     $exists: true
+  //   }
+  // }).toArray();
+  // console.log("ok");
+  // listRooms.forEach(room => {
+  //   room.list_users.forEach(sender_psid => {
+  //     client.db(dbName).collection('users-data').updateOne({ sender_psid: sender_psid }, {
+  //       $set: {
+  //         "room_chatting.block": false,
+  //         "room_chatting.has_joined": false,
+  //         "room_chatting.type": "",
+  //         "room_chatting.room_id": "",
+  //         "room_chatting.create_new_subroom": false
+  //       }
+  //     }, (err) => {
+  //       if(err) console.log(err);
+  //       else console.log(sender_psid + " ok");
+  //     });
+  //   });
+  //   client.db(dbName).collection("room-chatting").updateOne({
+  //     "room_id": room.room_id
+  //   }, {
+  //     $set: {
+  //       "list_users": []
+  //     }
+  //   }, (err) => {
+  //     if(err) console.log(err);
+  //     else console.log(room.room_id + " ok");
+  //   })
+  // });
 });
